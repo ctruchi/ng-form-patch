8fa8fcc69f5e3019a6a1de6b2302169b5b1c6a60
ctruchi <cyril.truchi@4sh.fr>
2023-01-25 11:09:16 +0100

    Buffer patch call between fields of the same service

--- projects/ng-form-patch/src/lib/services/patch.service.ts
+++ projects/ng-form-patch/src/lib/services/patch.service.ts
@@ -1,26 +1,46 @@
 import {Inject, Injectable} from '@angular/core';
 import {PatchServicesModule} from './patch-services.module';
 import {FORM_PATCH_SERVICE, FormPatchService} from '../ng-form-patch.module';
+import {BehaviorSubject, bufferTime, filter} from 'rxjs';
 
 @Injectable({
     providedIn: PatchServicesModule
 })
 export class PatchService {
 
+    public services: {
+        [key: string]: BehaviorSubject<PatchServiceArgs | undefined>
+    } = {}
+
     public constructor(@Inject(FORM_PATCH_SERVICE) private patchServices: FormPatchService[]) {
+        this.patchServices.forEach(service => {
+            let subject = new BehaviorSubject<PatchServiceArgs | undefined>(undefined);
+            this.services[service.patchKey] = subject;
+            subject
+                .pipe(
+                    filter(val => !!val),
+                    bufferTime(500)
+                )
+                .subscribe(args => {
+                    if (args !== undefined && args.length > 0) {
+                        service.patch(args[0]!.params, args.map(arg => ({path: arg!.path, value: arg!.value})))
+                            .subscribe();
+                    }
+                })
+        })
     }
 
     public patch(patch: string, patchPath: string, params: { [key: string]: any; }, value: any): void {
-        const service = this.getService(patch);
-
-        service.patch(params, [{path: patchPath, value: value}]).subscribe();
+        this.services[patch].next({
+            params: params,
+            path: patchPath,
+            value: value
+        });
     }
+}
 
-    private getService(patch: string): FormPatchService {
-        let service = this.patchServices.find(({patchKey}) => patchKey === patch);
-        if (!service) {
-            throw new Error(`No FormPatchService for ${patch}`);
-        }
-        return service;
-    }
+interface PatchServiceArgs {
+    params: { [key: string]: any };
+    path: string;
+    value: any;
 }
