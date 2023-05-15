043d0217a895fca8f07c57215056b59b3bb3c4c9
Adam <adam.zerdoumi@gmail.com>
2023-05-15 17:23:46 +0200

    Filter for ngModel dirty

--- projects/ng-form-patch/src/lib/patch-path.directive.ts
+++ projects/ng-form-patch/src/lib/patch-path.directive.ts
@@ -3,7 +3,7 @@ import {NgModel} from '@angular/forms';
 import {PatchParamDirective} from './patch-param.directive';
 import {PatchDirective} from './patch.directive';
 import {PatchService} from './services/patch.service';
-import {debounceTime} from 'rxjs';
+import {debounceTime, filter} from 'rxjs';
 
 export const DEBOUNCE_PER_FIELD = new InjectionToken<string>('DEBOUNCE_PER_FIELD');
 
@@ -25,7 +25,9 @@ export class PatchPathDirective {
         }
 
         ngModel.valueChanges!
-            .pipe(debounceTime(debouncePerField ?? 300))
+            .pipe(
+                filter(() => !!ngModel.dirty),
+                debounceTime(debouncePerField ?? 300))
             .subscribe(value => {
                 patchService.patch(patch.patch, this.patchPath, params.params, value)
             });
